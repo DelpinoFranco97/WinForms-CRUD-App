91f29484fe6875847d7de4f61ecf895694b8f267
delpinofranco <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>
2026-02-16 22:18:42 -0300

    Primer avance del tp final

---
+++ Tp final nivel 2/TPFinalNivel2_Delpino/Articulos/Articulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+
+{
+    
+    public class Articulo
+    {
+        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public decimal Precio { get; set; }
+        public Categoria NombreCategoria { get; set; }
+        public Marca Marca {  get; set; }
+
+        
+    
+
+    
+        }
+
+
+    }
+
---
+++ Tp final nivel 2/TPFinalNivel2_Delpino/CapaDatos/RepositorioArticulos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace CapaDatos
+{
+    public class RepositorioArticulos
+    {
+        public List<Articulo> ListaArticulos;
+        
+        public void Listar ()
+        {
+
+        }
+
+        public void AgregarArticulo()
+        {
+
+        }
+
+        public void ElimanarArticulo()
+        {
+
+        }
+
+        public void Modificar()
+        {
+
+        }
+    }
+}
