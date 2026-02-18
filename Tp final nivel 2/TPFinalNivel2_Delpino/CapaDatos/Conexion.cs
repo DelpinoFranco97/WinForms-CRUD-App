6c3f169026c692200a0b6f2cc921c134ffa750d4
delpinofranco <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>
2026-02-18 17:44:17 -0300

    Conexión a base de datos terminada

--- Tp final nivel 2/TPFinalNivel2_Delpino/CapaDatos/Conexion.cs
+++ Tp final nivel 2/TPFinalNivel2_Delpino/CapaDatos/Conexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,81 @@ namespace CapaDatos
 {
     public class Conexion
     {
+        // Declaracion de atributos de la clase.
+        private SqlCommand Command;
+        private SqlDataReader Reader;
+        private SqlConnection Connec;
+
+        public SqlDataReader ReaderConnection { get { return Reader; } }
+
+        public Conexion()
+        {
+            // Inicializacion de atributos de la clase.
+
+            Connec = new SqlConnection("server=.\\SQLEXPRESS; database = CATALOGO_DB; integrated security =true");
+            Command = new SqlCommand();
+        }
+
+        public void ejecutarLectura(string query)
+        {
+            VerificarConexion();
+            setearConsulta(query);
+            ejecutarReader();
+
+
+        }
+
+        public void setearConsulta(string query)
+        {
+            Command.CommandType = System.Data.CommandType.Text;
+            Command.CommandText = query;
+
+
+        }
+
+
+        public void ejecutarReader()
+        // Verifica  el estado del lector. Utilizo la propiedad IsClosed de sqlreader para obetener el valor. 
+
+        {
+            try
+            {
+                if (Reader != null && !Reader.IsClosed)
+                {
+                    Reader.Close();
+                }
+                Reader = Command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+        public void VerificarConexion()
+        {
+
+            // Verifica el estado de la conexion y lanza posibles errores para que otra capa lo maneje.
+            // Importante utilizo el namespace data y utilizo propiedad conectionstate que me permite saber el estaod de la conexion
+            try
+            {
+                Command.Connection = Connec;
+                if (Connec.State == System.Data.ConnectionState.Closed)
+                {
+                    Connec.Open();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+
+            }
+
+
+        }
+
     }
 }
+
--- Tp final nivel 2/TPFinalNivel2_Delpino/CapaDatos/RepositorioArticulos.cs
+++ Tp final nivel 2/TPFinalNivel2_Delpino/CapaDatos/RepositorioArticulos.cs
@@ -10,9 +10,34 @@ namespace CapaDatos
     public class RepositorioArticulos
     {
         public List<Articulo> ListaArticulos;
-        
-        public void Listar ()
+
+        public void Listar()
         {
+            List<Articulo> ListaArticulos = new List<Articulo>();
+            Conexion datos = new Conexion();
+
+            try
+            {
+                datos.ejecutarLectura("SELECT * FROM ARTICULOS");
+                while (datos.ReaderConnection.Read())
+                {
+                    Articulo articulo = new Articulo();
+                    articulo.Codigo = datos.ReaderConnection["Codigo"].ToString();
+                    articulo.Codigo = datos.ReaderConnection["Nombre"].ToString();
+                    articulo.Codigo = datos.ReaderConnection["Marca"].ToString();
+                    articulo.Codigo = datos.ReaderConnection["Categoria"].ToString();
+                    articulo.Codigo = datos.ReaderConnection["Url"].ToString();
+                    articulo.Precio = (decimal)datos.ReaderConnection["Precio"];
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
 
         }
 
